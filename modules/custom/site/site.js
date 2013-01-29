0faa3424ef52052b9c18589dafe63f2cef9ffe48
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2013-01-29 11:08:20 +0000

    add custom modules and features

---
+++ modules/custom/site/site.js
@@ -0,0 +1,14 @@
+(function ($) {
+  $(document).ready(function(){
+    //alert('dupa')
+
+    // amend style to fit popup window if javascript enabled - default made to look good without javascript
+    $('#form-challenge').hide();
+    $('div.proposal-popup').hide();
+    
+  });
+
+
+})(jQuery);
+
+
