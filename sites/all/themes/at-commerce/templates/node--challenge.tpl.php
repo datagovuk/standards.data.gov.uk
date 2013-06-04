73122d76de0ef04dd88c254dfb27fec097ee1d11
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2013-06-04 12:51:52 +0100

    Add close response and close proposal comments functionality

--- sites/all/themes/at-commerce/templates/node--challenge.tpl.php
+++ sites/all/themes/at-commerce/templates/node--challenge.tpl.php
@@ -1,6 +1,6 @@
 <?php
-
-  $open = $node->field_challenge_status['und'][0]['value'] == 1;
+  // TODO move logic to preprocess function
+  $open = $node->field_challenge_status['und'][0]['value'] == 1 && $node->field_response_close_date['und'][0]['value'] > time();
 
   global $base_url;
   if (user_is_anonymous()) {
@@ -12,6 +12,46 @@
 
   if($teaser){
     unset($content['links']);
+
+    if ($field_challenge_status['und'][0]['value'] == 1) {
+      // Building $challenge_status string only if challenge status == current
+      if (isset($field_response_close_date['und'][0]['value']) && $field_response_close_date['und'][0]['value'] > time()) {
+        $challenge_status = 'Challenge open for responses. Submit your response by ' . date('d/m/Y', $field_response_close_date['und'][0]['value']) ;
+      }
+      elseif (isset($field_close_comments['und'][0]['value'])) {
+        $challenge_status = 'Challenge closed for responses. ';
+
+        $sql = "SELECT *
+                FROM {field_data_field_proposal_phase} pp
+                JOIN {field_data_field_challenge_ref} chr
+                ON chr.entity_id = pp.entity_id
+                WHERE chr.field_challenge_ref_nid = $nid
+                AND pp.field_proposal_phase_value > 0
+                ";
+
+        $result = db_query($sql);
+
+
+        // if there are prpopsals with phase > 0 (not responses)
+        if ($result->rowCount()) {
+          if (isset($field_close_comments['und'][0]['value']) && $field_close_comments['und'][0]['value'] == 1) {
+            $challenge_status .= 'Proposal(s) open for comment.';
+          }
+          elseif (isset($field_close_comments['und'][0]['value'])) {
+            $challenge_status .= 'Proposal(s) closed for comment.';
+          }
+
+        }
+        else {
+          $challenge_status .= 'Proposal(s) in development.';
+        }
+
+      }
+    }
+
+
+
+
   }
 ?>
 
@@ -31,7 +71,7 @@
             <?php if ($page): ?>
               Challenge: <?php print $title; ?>
             <?php elseif (!$page): ?>
-              <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a>
+              <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a> <p class="challenge-status"><?php print $challenge_status; ?></p>
             <?php endif; ?>
           </h1>
         <?php endif; ?>
