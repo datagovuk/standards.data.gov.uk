0d8fd9488862fab1cdd5b333e3a90181c7d517a6
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-12-24 10:27:38 +0000

    Cleanup

--- sites/all/modules/custom/site/site.js
+++ sites/all/modules/custom/site/site.js
@@ -1,31 +1,23 @@
 (function ($) {
-  $(document).ready(function(){
-    //alert('dupa')
-
-  $('.form-item .description').each(function () {
+  $(document).ready(function () {
+    $('.form-item .description').each(function () {
       var desc = $(this);
       var label = desc.siblings('label:first');
-      var multTable, textArea;
-     if (label.length && !desc.parents('.field-multiple-table').length) {
-       desc.insertAfter(label);
-     } else if ((multTable = desc.siblings('.field-multiple-table')) && multTable.length) {
-         desc.insertAfter(multTable.find('label:first'));
-  }
-  });
-  $('.text-format-wrapper .description').each(function () {
-    var desc = $(this);
+      var multTable;
+      if (label.length && !desc.parents('.field-multiple-table').length) {
+        desc.insertAfter(label);
+      } else if ((multTable = desc.siblings('.field-multiple-table')) && multTable.length) {
+        desc.insertAfter(multTable.find('label:first'));
+      }
+    });
+    $('.text-format-wrapper .description').each(function () {
+      var desc = $(this);
       var label = desc.siblings('.form-item').find('label:first');
-     if (label.length) {
-          desc.insertAfter(label);
-     }
+      if (label.length) {
+        desc.insertAfter(label);
+      }
+    });
   });
-
-
-
-
-  });
-
-
 })(jQuery);
 
 
