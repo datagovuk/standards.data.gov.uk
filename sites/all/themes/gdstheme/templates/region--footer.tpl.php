79f3047523b39996aec07ff0cdf38c3a424c5932
David Otton <david@otton.org>
2015-01-27 17:40:22 +0000

    snapshot

---
+++ sites/all/themes/gdstheme/templates/region--footer.tpl.php
@@ -0,0 +1,14 @@
+<div class="footer-meta">
+  <div class="footer-meta-inner">
+    <?php if ($content): ?>
+      <div class="<?php print $classes; ?>">
+        <?php print $content; ?>
+      </div>
+    <?php endif; ?>
+    <div id="open-government-licence">
+      <p class="logo"><a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence</a></p>
+      <p class="ogl">All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</p>
+      <p class="crown-copyright"><a href="#">© Crown copyright</a></p>
+    </div>
+  </div>
+</div>
