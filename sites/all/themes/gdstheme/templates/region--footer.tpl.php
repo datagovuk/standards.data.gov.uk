24a95a7dde89eb2ab4efc72c1cc6d64d30602b6b
David Otton <david@otton.org>
2015-02-24 03:30:23 +0000

    Footer changes as per Katya

--- sites/all/themes/gdstheme/templates/region--footer.tpl.php
+++ sites/all/themes/gdstheme/templates/region--footer.tpl.php
@@ -1,10 +1,13 @@
 <div class="footer-meta">
   <div class="footer-meta-inner">
+    <div class="col1">
     <?php if ($content): ?>
+      <h2>More information</h2>
       <div class="<?php print $classes; ?>">
         <?php print $content; ?>
       </div>
     <?php endif; ?>
+    </div>
     <div id="open-government-licence">
       <p class="logo"><a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence</a></p>
       <p class="ogl">All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</p>
