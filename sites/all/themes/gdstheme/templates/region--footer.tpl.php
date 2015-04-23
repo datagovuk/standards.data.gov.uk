1e41c3031dcbc7367960c831917999976df0cfa2
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2015-04-23 17:19:40 +0100

    Fixes for responsive design

--- sites/all/themes/gdstheme/templates/region--footer.tpl.php
+++ sites/all/themes/gdstheme/templates/region--footer.tpl.php
@@ -5,10 +5,13 @@
         <?php print $content; ?>
       </div>
     </div>
+
+
     <div id="open-government-licence">
-      <p class="logo"><a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence</a></p>
-      <p class="ogl">All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated</p>
-      <p class="crown-copyright"><a href="http://www.nationalarchives.gov.uk/information-management/our-services/crown-copyright.htm">© Crown copyright</a></p>
+      <p class="logo"><a rel="license" href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence</a></p>
+      <p class="licence">All content is available under the <a rel="license" href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated</p>
     </div>
+    <div id="crown-copyright"><a href="http://www.nationalarchives.gov.uk/information-management/our-services/crown-copyright.htm">© Crown copyright</a></div>
+
   </div>
 </div>
