c1c4ec6725e5bc49c90daf0a56844016dfe69e1b
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2015-05-18 11:21:53 +0100

    Delete at-commerce theme

--- sites/all/themes/at-commerce/js/eq.fp.grid.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  Drupal.behaviors.ContentDisplayFrontPageGrid = {
-    attach: function(context) {
-      $("body.front #block-system-main").addClass("front-page-grid content-display-grid");
-      $("body.front #block-system-main .article-inner").equalHeight();
-    }
-  }
-})(jQuery);
